ed6beee65ce86da4095f109797cf18058e5e2e34
Stefanni <stefannibrasil@gmail.com>
2018-04-23 09:47:21 -0700

    D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :avatar, presence: true
   validates :title, uniqueness: true, presence: true, length: { minimum: 10 }
   validates :description, presence: true
@@ -9,4 +13,14 @@ class Product < ApplicationRecord
 
   validates_attachment_content_type :avatar, content_type: /\Aimage/
   validates_attachment_file_name :avatar, matches: [/.png\Z/i, /.jpe?g\Z/i, /.gif\Z/i]
+
+  
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
