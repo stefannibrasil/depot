9c351434c6dec832f648ed088ff47729353a2f41
Stefanni <stefannibrasil@gmail.com>
2018-05-04 18:55:15 -0700

    Task I: Processing Emails and Payments Efficiently

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,7 +1,15 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
   test "check routing number" do
+    # Since you now need to submit the form and assert that
+    # an order was created, you need to clear out any orders in the test database
+    # that might be hanging around from previous test runs:
+    LineItem.delete_all
+    Order.delete_all
+
     visit store_index_url
 
     first('.catalog li').click_on 'Add to Cart'
@@ -15,5 +23,28 @@ class OrdersTest < ApplicationSystemTestCase
     assert_no_selector "#order_routing_number"
 
     assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Bob Doe", order.name
+    assert_equal "My String",  order.address
+    assert_equal "bob@doe.com", order.email
+    assert_equal "Check", order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["bob@doe.com"], mail.to
+    assert_equal 'bob@doe.com', mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
 end
